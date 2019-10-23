560dc404545f7d6a50ee3ef8174509e05cc837ea
Miki Nomura <nomura_jpngm@live.jp>
2019-10-24 09:23:23 +1300

    Update animation_imageFiles.R

--- animation_imageFiles.R
+++ animation_imageFiles.R
@@ -1,8 +1,9 @@
 
 ### Create new directory and store all image files you want to put in the animation
 ### You must not have any other image files in the directory.
-setwd("Y://New folder")
+setwd("Y://New folder//global map")
 
+list.files()
 
 # convert the image (.tif) files to one .gif file using ImageMagick. 
 # The system() function executes the command as if it was done
@@ -12,10 +13,27 @@ setwd("Y://New folder")
 ### Use magick instead.
 ### https://stackoverflow.com/questions/38163849/imagemagick-issue-with-windows-and-convert-function
 
-system("magick -delay 80 *.tif example_1.gif")
-
-# to not leave the directory with the single png files
-# I remove them.
-file.remove(list.files(pattern=".png"))
+system("magick -delay 80 *.png example_1.gif")
 
 ### The created .gif file is the animation file.
+
+
+dir.create("examples")
+setwd("examples")
+
+# example 1: simple animated countdown from 10 to "GO!".
+png(file="example%02d.png", width=200, height=200)
+for (i in c(10:1, "G0!")){
+  plot.new()
+  text(.5, .5, i, cex = 6)
+}
+dev.off()
+
+list.files()
+# convert the .png files to one .gif file using ImageMagick. 
+# The system() function executes the command as if it was done
+# in the terminal. the -delay flag sets the time between showing
+# the frames, i.e. the speed of the animation.
+system("magick -delay 80 *.png example_1.gif")
+
+
